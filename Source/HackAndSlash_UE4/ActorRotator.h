c360cde3b461f5ccf5593fd3ffa797fc6df752eb
Will Roberts <willroberts3@protonmail.com>
2023-02-24 20:30:21 -0500

    Adds C++ project files

---
+++ Source/HackAndSlash_UE4/ActorRotator.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "ActorRotator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HACKANDSLASH_UE4_API UActorRotator : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
